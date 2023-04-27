c257f7bc9e238b97ea011b0e8f3d59941844a055
Felipe Barreto <felipebarreto1050@gmail.com>
2023-04-26 22:39:19 -0300

    Install Styled-Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { StyleSheet, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import styled from 'styled-components/native'
+
+const Button = styled.TouchableHighlight`
+  background-color: red;
+  height: 40;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
 const App:React.FC = () => {
   return (
     <SafeAreaView>
       <Text>App</Text>
+      <Button><Text>Comprar</Text></Button>
     </SafeAreaView>
   )
 }
