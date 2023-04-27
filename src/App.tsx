8d4cc468b54b682e5552ca6b7f922f0334330fc4
Felipe Barreto <felipebarreto1050@gmail.com>
2023-04-26 22:09:05 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const App:React.FC = () => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
